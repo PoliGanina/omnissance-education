f93ac815523c42686ba2a768d1fa66b142855569
Polina Ganina <poliganina@gmail.com>
2022-02-19 00:24:15 +0300

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -11,22 +11,52 @@ $(document).ready(function() {
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-  const menu = document.querySelector('.dropdown'),
-  header = document.querySelector('.header'),
-  menuItem = document.querySelectorAll('.dropdown-column__link'),
-  dropdownLink = document.querySelector('.header-menu__dropdown');
+  let list = document.querySelector('.dropdown'),
+      header = document.querySelector('.header'),
+      listItem = document.querySelectorAll('.dropdown-column__link'),
+      dropdownLink = document.querySelector('.header-list__dropdown');
 
   dropdownLink.addEventListener('click', () => {
-    dropdownLink.classList.toggle('header-menu__dropdown_active');
-      menu.classList.toggle('dropdown_active');
-      header.classList.toggle('header_active');
+    dropdownLink.classList.toggle('header-list__dropdown_active');
+      list.classList.toggle('dropdown_active');
+      header.classList.add('header_active');
   });
 
-  menuItem.forEach(item => {
+  listItem.forEach(item => {
       item.addEventListener('click', () => {
-        dropdownLink.classList.toggle('header-menu__dropdown_active');
-          menu.classList.toggle('dropdown_active');
+        dropdownLink.classList.toggle('header-list__dropdown_active');
+          list.classList.toggle('dropdown_active');
           header.classList.toggle('header_active');
-      })
-  })
-})
+      });
+  });
+});
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   const menu = document.querySelector('.header-list'),
+//   header = document.querySelector('.header'),
+//   listItem = document.querySelectorAll('.header-list__link'),
+//   dropdownLink = document.querySelector('.hamburger');
+
+//   dropdownLink.addEventListener('click', () => {
+//     // dropdownLink.classList.toggle('hamburger_active');
+//       menu.classList.toggle('header-list_active');
+//       header.classList.toggle('header_active');
+//   });
+
+//   listItem.forEach(item => {
+//       item.addEventListener('click', () => {
+//         // dropdownLink.classList.toggle('hamburger_active');
+//           menu.classList.toggle('header-list_active');
+//           header.classList.toggle('header_active');
+//       });
+//   });
+// });
+
+let   hamburger = document.querySelector ('.hamburger'),
+      menu = document.querySelector ('.header-list'),
+      overlay = document.querySelector ('.header');
+
+hamburger.addEventListener('click', () => {
+    overlay.classList.add('header_active'),
+    menu.classList.add('header-list_active');
+});
